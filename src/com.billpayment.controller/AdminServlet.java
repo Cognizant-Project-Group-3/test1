19f712b49dc1210c3f3e85e50ebcc64a059bbd46
mshekdar <shekdarm@gmail.com>
2015-12-08 10:09:13 +0530

    Create AdminServlet.java

---
+++ src/com.billpayment.controller/AdminServlet.java
@@ -0,0 +1,50 @@
+package com.billpayment.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//Code to verify admin userid and password
+		
+		
+		//If Admin
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("If you are a customer <a href='Customer.html'>CLICK HERE</a>");
+		out.print("If you are a customer <a href='Vendor.html'>CLICK HERE</a>");
+		
+		//If not Admin redirect to index page
+	}
+
+}
